aace318c731dfd53bf18ba2085369a7458c17ab8
Sriharsha2001 <53521201+Sriharsha2001@users.noreply.github.com>
2025-05-16 14:05:16 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,7 +1,13 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "ecommerce","3306")
-    or die(mysqli_error($con));
-    if(!isset($_SESSION)){
-      session_start();
+    $con = mysqli_connect(
+        getenv('DB_HOST'),
+        getenv('DB_USER'),
+        getenv('DB_PASS'),
+        getenv('DB_NAME'),
+        getenv('DB_PORT')
+    ) or die(mysqli_error($con));
+
+    if (!isset($_SESSION)) {
+        session_start();
     }
 ?>
