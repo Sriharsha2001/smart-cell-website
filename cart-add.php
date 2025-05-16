60165ddaa6db509d28aab4027e065a231c7eb3d0
Sriharsha2001 <53521201+Sriharsha2001@users.noreply.github.com>
2025-05-16 15:42:12 +0530

    Update cart-add.php

--- cart-add.php
+++ cart-add.php
@@ -1,10 +1,22 @@
 <?php
 require("includes/common.php");
+
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $item_id = $_GET['id'];
     $user_id = $_SESSION['user_id'];
+
+    // Escape variables
+    $item_id = pg_escape_string($con, $item_id);
+    $user_id = pg_escape_string($con, $user_id);
+
     $query = "INSERT INTO users_items(user_id, item_id, status) VALUES('$user_id', '$item_id', 'Added to cart')";
-    mysqli_query($con, $query) or die(mysqli_error($con));
-    header('location: products.php');
+    $result = pg_query($con, $query);
+
+    if (!$result) {
+        die("Error in query: " . pg_last_error($con));
+    }
+
+    header('Location: products.php');
+    exit();
 }
 ?>
