dcf32a94518c18506f4d90b9c9053b393341eb18
Sriharsha2001 <53521201+Sriharsha2001@users.noreply.github.com>
2025-05-16 15:44:13 +0530

    Update check-if-added.php

--- includes/check-if-added.php
+++ includes/check-if-added.php
@@ -1,20 +1,27 @@
 <?php
 
-//This code checks if the product is added to cart. 
+// This code checks if the product is added to cart. 
 function check_if_added_to_cart($item_id) {
-    $user_id = $_SESSION['user_id']; //We'll get the user_id from the session
-    require("common.php"); // connecting to the database
-    // We will select all the entries from the user_items table where the item_id is equal to the item_id we passed to this function, user_id is equal to the user_id in the session and status is 'Added to cart'
-    
-    $query = "SELECT * FROM users_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
-    $result = mysqli_query($con, $query) or die(mysqli_error($con));
-    
-// We'll check if the no.of rows in the result and no.of rows returned by the mysqli_num_rows($result) is true. If yes then it return 0 else it returns 0
-    if (mysqli_num_rows($result) >= 1) {
+    $user_id = $_SESSION['user_id']; // We'll get the user_id from the session
+    require("common.php"); // Connecting to the database
+
+    // Sanitize inputs
+    $item_id = pg_escape_string($con, $item_id);
+    $user_id = pg_escape_string($con, $user_id);
+
+    // Query to check if item is already in the user's cart with status 'Added to cart'
+    $query = "SELECT * FROM users_items WHERE item_id='$item_id' AND user_id='$user_id' AND status='Added to cart'";
+    $result = pg_query($con, $query);
+
+    if (!$result) {
+        die("Error in query: " . pg_last_error($con));
+    }
+
+    // Return 1 if item is found, else 0
+    if (pg_num_rows($result) >= 1) {
         return 1;
     } else {
         return 0;
     }
 }
-
 ?>
