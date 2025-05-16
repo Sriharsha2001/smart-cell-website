0a7295fa4d4ef645d6ed734b323942ee16f5e8d9
Sriharsha2001 <53521201+Sriharsha2001@users.noreply.github.com>
2025-05-16 15:43:00 +0530

    Update cart-remove.php

--- cart-remove.php
+++ cart-remove.php
@@ -1,12 +1,19 @@
 <?php
 require("includes/common.php");
-if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-    $item_id = $_GET["id"];
-    $user_id = $_SESSION['user_id'];
 
-    // Delete the rows from user_items table where item_id and user_id equal to the item_id and user_id we got from the cart page and session
-    $query = "DELETE FROM users_items WHERE item_id='$item_id' AND user_id='$user_id' ";
-    $res = mysqli_query($con, $query) or die($mysqli_error($con));
-    header("location:cart.php");
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $item_id = pg_escape_string($con, $_GET["id"]);
+    $user_id = pg_escape_string($con, $_SESSION['user_id']);
+
+    // Delete the rows from users_items table where item_id and user_id match
+    $query = "DELETE FROM users_items WHERE item_id='$item_id' AND user_id='$user_id'";
+    $res = pg_query($con, $query);
+
+    if (!$res) {
+        die("Error in query: " . pg_last_error($con));
+    }
+
+    header("Location: cart.php");
+    exit();
 }
 ?>
